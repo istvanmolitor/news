5273b30f995a7b9804de0b3982ffdff086478007
Molitor István <istvanmolitor@gmail.com>
2026-06-23 14:44:55 +0200

    language select

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,9 +5,11 @@ use Molitor\News\Http\Controllers\AuthorController;
 use Molitor\News\Http\Controllers\NewsController;
 use Molitor\News\Http\Controllers\SearchController;
 
-Route::get('/', [NewsController::class, 'homepage'])->name('news.homepage');
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
-Route::get('/authors', [AuthorController::class, 'index'])->name('news.authors');
-Route::get('/authors/{slug}', [AuthorController::class, 'show'])->name('news.author.show');
-Route::get('/search', SearchController::class)->name('news.search');
+Route::middleware(['web'])->group(function () {
+    Route::get('/', [NewsController::class, 'homepage'])->name('news.homepage');
+    Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
+    Route::get('/authors', [AuthorController::class, 'index'])->name('news.authors');
+    Route::get('/authors/{slug}', [AuthorController::class, 'show'])->name('news.author.show');
+    Route::get('/search', SearchController::class)->name('news.search');
+});
